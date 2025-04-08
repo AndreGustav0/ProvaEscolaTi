851c52f97b30343edaa3d8e0c67bceabdc09df73
AndreGustav0 <andregustavodeoliveira49@gmail.com>
2025-04-08 20:06:56 -0300

    Preparando ambiente de trabalho

---
+++ src/main/java/Prova/EscolaTi/EscolaTiApplication.java
@@ -0,0 +1,13 @@
+package Prova.EscolaTi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaTiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Prova/EscolaTi/EscolaTiApplicationTests.java
@@ -0,0 +1,13 @@
+package Prova.EscolaTi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
