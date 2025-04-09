b4e2f73b704b983c7b4d57c89312c0bf23596bf4
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-04-09 13:42:58 -0300

    Adicionando Lombok e suas funcionalidades nas entidades

--- src/main/java/Prova/EscolaTi/entities/ItemMagico.java
+++ src/main/java/Prova/EscolaTi/entities/ItemMagico.java
@@ -1,11 +1,19 @@
 package Prova.EscolaTi.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_itemMagico")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ItemMagico {
 
     @Id
--- src/main/java/Prova/EscolaTi/entities/Personagem.java
+++ src/main/java/Prova/EscolaTi/entities/Personagem.java
@@ -1,12 +1,20 @@
 package Prova.EscolaTi.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tb_personagem")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Personagem {
 
     @Id
